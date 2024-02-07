2cee4618058f333e7581237849dbcfbcadbcc8d4
Ahmet <110814262+zapkinus11@users.noreply.github.com>
2024-02-07 16:09:23 +0300

    not_hesaplama_Cpp
    c++ diliyle yazılmış vize ve final notunuzu hesaplama kodu (vize %40, final %60 baz alınmıştır!)

---
+++ notHesapla/notHesapla/notHesapla.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+
+	int not1, not2, ortalama;
+
+	cout << "Vize notunuzu giriniz: ";
+	cin >> not1;
+
+	cout << "Final notunuzu giriniz: ";
+	cin >> not2;
+
+	ortalama = not1 * 0.4 + not2 * 0.6;
+
+	if (ortalama >= 50)
+	{
+		cout << " " << "\n";
+		cout << "Vize ve Final not Ortalamanız = " << ortalama << " Tebrikler dersi başarıyla geçtiniz."<<"\n";
+		cout << " ";
+	}
+	else {
+		cout << " "<<"\n";
+		cout << "Vize ve Final not Ortalamanız = "<< ortalama<< " Dersten kaldınız!"<<"\n";
+		cout << " ";
+	}
+}
